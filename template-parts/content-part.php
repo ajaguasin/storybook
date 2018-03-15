6952adfc113b98d623ea99836123b67f2fb64dd8
ajaguasin <ajaguasin@gmail.com>
2018-03-15 04:57:38 -0700

    Tiny commit

--- template-parts/content-part.php
+++ template-parts/content-part.php
@@ -2,8 +2,8 @@
 
         if(have_posts()):
             while ( have_posts()): the_post(); ?>
-                <h3><?php the_title(); ?></h3>
-                <p><?php the_content(); ?></p> 
+                <h3 class="home-content"><?php the_title(); ?></h3>
+                <p class="home-content"><?php the_content(); ?></p> 
 
             <?php endwhile;
         endif;
